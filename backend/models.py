d551e7497e14a31e60b8f5a6b2dc95a30888d2ab
Rodrigo <rodericus@alu.ufc.br>
2024-10-18 14:59:42 -0300

    adjustment in sql query

--- backend/models.py
+++ backend/models.py
@@ -3,11 +3,15 @@ from database import get_db_connection
 def fetch_suppliers():
     connection = get_db_connection()
     cursor = connection.cursor()
-    query = 'SELECT DISTINCT Fantasia AS nome FROM vw_infos'
+    query = """
+        SELECT Fantasia AS nome, Descricao AS categoria
+        FROM vw_fabri_cat
+        WHERE Descricao LIKE '%Farma%'
+    """
     cursor.execute(query)
     result = cursor.fetchall()
 
-    suppliers = [{'nome': row.nome} for row in result]
+    suppliers = [{'nome': row.nome, 'categoria': row.categoria} for row in result]
 
     cursor.close()
     connection.close()
