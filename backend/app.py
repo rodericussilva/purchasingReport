1bc5bc0443d2106d31066ee16437e0db252ae02b
Rodrigo <rodericus@alu.ufc.br>
2024-10-28 14:53:27 -0300

    adjustment in data provision

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,7 @@
 import os
 from flask import Flask, jsonify, request
 from flask_cors import CORS
-from models import fetch_suppliers, fetch_product_supplires, fetch_month_abbreviations
+from models import fetch_suppliers, fetch_product_suppliers
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -24,15 +24,10 @@ def get_products():
         if not supplier_name:
             return jsonify({'error': 'Fornecedor não especificado'}), 400
         
-        produtos = fetch_product_supplires(supplier_name)
+        produtos = fetch_product_suppliers(supplier_name)
         return jsonify(produtos), 200
     except Exception as e:
         return jsonify({'error': str(e)}), 500
-    
-@app.route('/api/month-abbreviations', methods=['GET'])
-def get_month_abbreviations():
-    month_abbreviations = fetch_month_abbreviations()
-    return jsonify(month_abbreviations)
 
 if __name__ == '__main__':
     host = os.getenv('FLASK_HOST', '127.0.0.1')
--- backend/models.py
+++ backend/models.py
@@ -5,7 +5,7 @@ def fetch_suppliers():
     cursor = connection.cursor()
     query = """
         SELECT DISTINCT Fantasia AS nome
-        FROM vw_fabri_produ
+        FROM vw_fabric_produ
         ORDER BY Fantasia ASC
     """
     cursor.execute(query)
@@ -17,20 +17,31 @@ def fetch_suppliers():
     connection.close()
     return suppliers
 
-def fetch_product_supplires(supplier_name):
+def fetch_product_suppliers(supplier_name):
     connection = get_db_connection()
     cursor = connection.cursor()
 
     query = """
-        SELECT DISTINCT
-            Desc_Prod AS descricao,
-            Qtd_VenMes0,
-            Qtd_VenMes1,
-            Qtd_VenMes2,
-            Sta_AbcUniVenFab
-        FROM vw_fabri_produt
+        SELECT  
+            Descricao,
+            Qtd_FatMes0,
+            Qtd_FatMes1,
+            Qtd_FatMes2,
+            Qtd_FatMes3,
+            Media_Fat,
+            Qtd_EstMin,
+            Qtd_Ressup,
+            Qtd_Dispon,
+            Qtd_Sugest,
+            Prc_UniCom,
+            Sta_AbcUniVenFab,
+            Des_VenMes0,
+            Des_VenMes1,
+            Des_VenMes2,
+            Des_VenMes3
+        FROM vw_fabric_produ
         WHERE Fantasia = ?
-        ORDER BY descricao ASC
+        ORDER BY Descricao ASC
     """
     cursor.execute(query, (supplier_name,))
     result = cursor.fetchall()
@@ -38,37 +49,26 @@ def fetch_product_supplires(supplier_name):
     products = []
     for row in result:
         products.append({
-            'descricao': row.descricao,
-            'unidades_faturadas_mes1': row.Qtd_VenMes0,
-            'unidades_faturadas_mes2': row.Qtd_VenMes1,
-            'unidades_faturadas_mes3': row.Qtd_VenMes2,
+            'descricao': row.Descricao,
+            'unidades_faturadas_mes0': row.Qtd_FatMes0,
+            'unidades_faturadas_mes1': row.Qtd_FatMes1,
+            'unidades_faturadas_mes2': row.Qtd_FatMes2,
+            'unidades_faturadas_mes2': row.Qtd_FatMes3,
+            'media_faturada': row.Media_Fat,
+            'estoque_minimo': row.Qtd_EstMin,
+            'estoque_suprimento': row.Qtd_Ressup,
+            'estoque_disponivel': row.Qtd_Dispon,
+            'sugestao_compra': row.Qtd_Sugest,
+            'valor_compra': row.Prc_UniCom,
             'curva': row.Sta_AbcUniVenFab,
+            'mes_labels': {
+                'mes0': row.Des_VenMes0,
+                'mes1': row.Des_VenMes1,
+                'mes2': row.Des_VenMes2,
+                'mes3': row.Des_VenMes3,
+            }
         })
 
     cursor.close()
     connection.close()
     return products
-
-def fetch_month_abbreviations():
-    connection = get_db_connection()
-    cursor = connection.cursor()
-    
-    query = """
-        SELECT 
-            Des_VenMes2 AS mes3,
-            Des_VenMes1 AS mes2,
-            Des_VenMes0 AS mes1
-        FROM vw_fabri_produt
-    """
-    
-    cursor.execute(query)
-    result = cursor.fetchone()
-    
-    cursor.close()
-    connection.close()
-    
-    return {
-        'mes3': result.mes3,
-        'mes2': result.mes2,
-        'mes1': result.mes1
-    }
